5a4b6f9d02af8a695ff6c7958c934df4bb1f0f3e
Cittu S L <slcittu@gmail.com>
2024-10-03 09:50:39 +0530

    Updated program header

--- inputwithinrange.c
+++ inputwithinrange.c
@@ -1,3 +1,10 @@
+/*
+Program header 
+1.Objective : Created a code to input a number between a range using logical oprators
+2.Revisions: Nil
+3.Date & time of revision :03/10/24
+4.Author Name: Cittu S L
+*/
 #include<stdio.h>
 
 void main()
