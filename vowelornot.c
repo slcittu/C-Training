aa242935a61a569399efcacb1b6e842b8f6e86dd
Cittu S L <slcittu@gmail.com>
2024-10-03 10:13:05 +0530

    added the feature to check if user input is an alphabet or not

--- vowelornot.c
+++ vowelornot.c
@@ -13,14 +13,16 @@ void main()
     char ch;
     printf("Enter a character:");
     scanf(" %c",&ch);
+  if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+  {
+        if(ch=='A'||ch=='E'|| ch=='I'|| ch=='O' || ch=='U'|| ch =='a'|| ch=='e'|| ch=='i' ||ch=='o'||ch=='u')
+        {
+            printf("The character you have entered is a Vowel: %c", ch);
+        }
+        else
+        {
+            printf("The character you have entered is not a Vowel: %c", ch);
 
-    if(ch=='A'||ch=='E'|| ch=='I'|| ch=='O' || ch=='U'|| ch =='a'|| ch=='e'|| ch=='i' ||ch=='o'||ch=='u')
-    {
-        printf("The character you have entered is a Vowel: %c", ch);
-    }
-    else
-    {
-        printf("The character you have entered is not a Vowel: %c", ch);
-
-    }
+        }
+  } 
 }
