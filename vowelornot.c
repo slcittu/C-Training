c8f5d27cf6f84f3371d760285889841b0f4195e2
Cittu S L <slcittu@gmail.com>
2024-10-03 10:06:04 +0530

    Write a code to check iuf character entered by user is a Vowel.

---
+++ vowelornot.c
@@ -0,0 +1,26 @@
+/*
+Program header 
+1.Objective : Write a code to check iuf character entered by user is a Vowel.
+2.Revisions: Nil
+3.Date & time of revision :02/10/24
+4.Author Name: Cittu S L
+*/
+
+#include<stdio.h>
+
+void main()
+{
+    char ch;
+    printf("Enter a character:");
+    scanf(" %c",&ch);
+
+    if(ch=='A'||ch=='E'|| ch=='I'|| ch=='O' || ch=='U'|| ch =='a'|| ch=='e'|| ch=='i' ||ch=='o'||ch=='u')
+    {
+        printf("The character you have entered is a Vowel: %c", ch);
+    }
+    else
+    {
+        printf("The character you have entered is not a Vowel: %c", ch);
+
+    }
+}
