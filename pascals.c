ce77c19ee3ce361a70f4c66c416642b648db3406
Cittu S L <slcittu@gmail.com>
2024-10-04 15:48:52 +0530

    Implementing Pascal's Triangle

---
+++ pascals.c
@@ -0,0 +1,30 @@
+/*
+Program header 
+1.Objective : Implementing Pascal's Triangle
+ 2.Revisions: Nil
+3.Date & time of revision :04/10/24
+4.Author Name: Cittu S L
+*/
+
+#include <stdio.h>
+
+int main() {
+    int numRows=5, row, col, space;
+    long coeff;
+    for (row = 0; row < numRows; row++) 
+    {
+
+        for (space = 0; space < numRows - row - 1; space++) {
+            printf("  ");
+        }
+        coeff = 1; 
+        for (col = 0; col <= row; col++) 
+        {
+            printf("%4ld", coeff); 
+            coeff = coeff * (row - col) / (col + 1);
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
