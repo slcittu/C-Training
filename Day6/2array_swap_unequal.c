46fb653e5ff114aed13384c7c7a90299a482bb18
Cittu S L <slcittu@gmail.com>
2024-10-08 15:12:31 +0530

    code to swap 2 arrays of unequal size

--- Day6/2array_swap_unequal.c
+++ Day6/2array_swap_unequal.c
@@ -12,18 +12,12 @@ Program header
 
 int main()
 {
-    int size1,size2,arr1[25],arr2[25],index;
+    int size1,size2,arr1[25],arr2[25],index,limit=0;
 
     printf("Enter the size of array 1: ");
     scanf("%d",&size1);
     printf("Enter the size of array 2: ");
     scanf("%d",&size2);
-
-    if(size1!=size2)
-    {
-        printf("Size of arrays not matching!!!\n Terminating Program...\n");
-        return 0;
-    }
 
     for(index=0;index<size1;index++)
     {
@@ -31,24 +25,51 @@ int main()
         scanf("%d",&arr1[index]);
     }
 
-    for(index=0;index<size1;index++)
+    for(index=0;index<size2;index++)
     {
         printf("Enter the %d element for Array 2: ",index+1);
         scanf("%d",&arr2[index]);
     }
 
-    for(index=0;index<size1;index++)
+    if(size1!=size2)
     {
-        if(arr2[index]!=0)
-        {
-            arr1[index]=arr1[index]*arr2[size2-1-index];
-            arr2[size2-1-index]=arr1[index]/arr2[size2-1-index];
-            arr1[index]=arr1[index]/arr2[size2-1-index];
+        if (size1<size2) 
+        {   
+            limit=size2;
+            for(index=size1;index<size2;index++) 
+            {
+                arr1[index]=0;
+            }
         }
-        
-       
-    }
+        else
+        {
+            limit=size1;
+            for(index=size2;index<size1;index++)
+            {
+                arr2[index]=0;
+            }
+        }
 
+        
+        for(index=0;index<limit;index++)
+        {
+            arr1[index]=arr1[index]+arr2[limit-1-index];
+            arr2[limit-1-index]=arr1[index]-arr2[limit-1-index];
+            arr1[index]=arr1[index]-arr2[limit-1-index];
+        }      
+    }
+    else
+    {
+        for(index=0;index<size1;index++)
+        {
+            if(arr2[index]!=0)
+            {
+                arr1[index]=arr1[index]*arr2[size2-1-index];
+                arr2[size2-1-index]=arr1[index]/arr2[size2-1-index];
+                arr1[index]=arr1[index]/arr2[size2-1-index];
+            }
+        }
+    }
     printf("\nArrays After ");
     printf("\nSwapped arrays:\n");
     printf("Array 1: ");
@@ -63,7 +84,7 @@ int main()
     }
     printf("\n");
 
-return 0;
+    return 0;
 
 
 }
